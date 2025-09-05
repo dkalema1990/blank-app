2fa4823acaf91bfbe3bff827aee853ae0b7d6ebd
dkalema1990 <90250412+dkalema1990@users.noreply.github.com>
2025-09-05 11:58:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title('Uber pickups in NYC')
+
+streamlit run uber_pickups.py
